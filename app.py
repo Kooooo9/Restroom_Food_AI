47ed5ccf1d6d5c9c8148c65220bc8a21c9e0b83f
younghun <disrespect901@gmail.com>
2025-10-23 16:12:26 +0900

    slider

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def main():
+    st.subheader('AI 식단 생성')
+    
+    st.number_input('목표 칼로리 (kcal)', 1000, 6000, 2500, step=50)
+
+    st.slider('탄수화물 (%)', 10, 80, 50)
+    st.slider('단백질 (%)', 10, 50, 30)
+    st.slider('지방 (%)', 10, 50, 20)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
